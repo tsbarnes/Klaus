890d39c2a117a4420ef6f4809927b0f6db0a2419
Thea Barnes <thea@tsbarnes.com>
2020-10-20 17:33:33 -0400

    Borrowed some SCSS files from another theme

--- gulpfile.js
+++ gulpfile.js
@@ -11,26 +11,24 @@ gulp.task('gnome-shell', function() {
 });
 
 gulp.task('gtk-light', function() {
-  return gulp.src('sass/gtk-light/gtk-light.scss')
+  return gulp.src('sass/gtk-3.0/gtk-light.scss')
     .pipe(sass({
       style: 'expanded'
     }))
-    .pipe(gulp.dest('gtk-3.20/dist'))
+    .pipe(gulp.dest('gtk-3.0/dist'))
 });
 
 gulp.task('gtk-dark', function() {
-  return gulp.src('sass/gtk-dark/gtk-dark.scss')
+  return gulp.src('sass/gtk-3.0/gtk-dark.scss')
     .pipe(sass({
       style: 'expanded'
     }))
-    .pipe(gulp.dest('gtk-3.20/dist'))
+    .pipe(gulp.dest('gtk-3.0/dist'))
 });
 
 gulp.task('watch', function () {
-  gulp.watch('sass/gtk-light/**/*.scss', gulp.series(['gtk-light']));
-  gulp.watch('sass/gtk-dark/**/*.scss', gulp.series(['gtk-dark']));
+  gulp.watch('sass/gtk-3.0/**/*.scss', gulp.series(['gtk-light', 'gtk-dark']));
   gulp.watch('sass/gnome-shell/**/*.scss', gulp.series(['gnome-shell']));
 });
 
 gulp.task('default', gulp.series(['gtk-light', 'gtk-dark', 'gnome-shell', 'watch']));
-
